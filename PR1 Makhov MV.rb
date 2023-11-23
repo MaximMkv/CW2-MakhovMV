01871c1c1131a2575311a828d43052bfcdd22443
MaximMkv <maximmahov78@gmail.com>
2023-11-23 22:59:56 +0200

    Create PR1 Makhov MV.rb

---
+++ PR1 Makhov MV.rb
@@ -0,0 +1,46 @@
+module Printable
+  def print_info
+    puts "Information about the objects:"
+    puts "\nClass: #{self.class}"
+    instance_variables.each do |var|
+      puts "#{var}: #{instance_variable_get(var)}"
+    end
+  end
+end
+
+
+
+class Person
+  include Printable # використання у класі Person
+
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+
+
+class Car
+  include Printable # Викоритсання у класі Car
+
+  attr_accessor :make, :model, :year
+
+  def initialize(make, model, year)
+    @make = make
+    @model = model
+    @year = year
+  end
+end
+
+
+person = Person.new("John", 22)
+person.print_info
+
+
+car = Car.new("Nissan", "Navara", 2019)
+car.print_info
+
+
